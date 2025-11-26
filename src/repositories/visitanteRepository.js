958e8d6aaaa7067c87a3bf943eb772bb63b08b9d
Miguel Hernandez <jesmiguelhdzg@gmail.com>
2025-11-26 08:17:20 -0600

    Registro y obtencion de visitantes

--- src/repositories/visitanteRepository.js
+++ src/repositories/visitanteRepository.js
@@ -9,56 +9,16 @@ class VisitanteRepository extends BaseRepository {
         super(visitante);
     }
 
-    async findByName(nombre){
-        return await this.model.findOne({ where: { nombre } });
+    async findAllVisitantes({limit = 10, offset = 0}) {
+        return await this.model.findAndCountAll({limit, offset});
     }
 
-    async findById(id){
-        return await this.findById(id);
-    }
-
-    async findAllVisitantes(){
-        return await this.findAll();
-    }
-     
-    async findByEmail(email){
-        return await this.model.findOne({ where: { email } });
-    }
-
-   
-    async findGroupVisitors(){
-        return await this.model.findAll({ where: { grupo: true } });
-    }
-
-    
-    async findIndividualVisitors(){
-        return await this.model.findAll({ where: { grupo: false } });
-    }
-
-    
-    async findByCountry(pais){
-        return await this.model.findAll({ where: { pais } });
-    }
-
-    
-    async findByGender(genero){
-        return await this.model.findAll({ where: { genero } });
-    }
-
-    async createVisitor(data){
-        return await this.create(data);
-    }
-
-    async updateVisitor(id, data){
-        const visitante = await this.findById(id);
-        if(!visitante) throw new Error("Visitante no encontrado");
-        return await visitante.update(data);
-    }
-
-    async deleteVisitor(id){
-        const visitante = await this.findById(id);
-        if(!visitante) throw new Error("Visitante no encontrado");
-        return await visitante.destroy();
+    async findAllVisitantesByMuseoId({museoId, limit = 10, offset = 0}) {
+        return await this.model.findAndCountAll({
+            where: { museoId },
+            limit,
+            offset
+        });
     }
 
 }
